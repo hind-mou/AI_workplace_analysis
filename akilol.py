985082f19875048e44d450816a8fcb244f949796
hind-mou <moutaoikilhind@gmail.com>
2025-10-21 11:59:36 +0100

    with py file

---
+++ akilol.py
@@ -0,0 +1,11 @@
+# Python script to print a rabbit face without symbols (just text)
+
+def print_rabbit():
+    rabbit_face = """
+      Rabbit
+       Face
+    """
+    print(rabbit_face)
+
+if __name__ == "__main__":
+    print_rabbit()
